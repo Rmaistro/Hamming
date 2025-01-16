be52e84a7d738f6105dd50be186b766579a53a7d
Rmaistro <baaijensxander@gmail.com>
2025-01-16 13:51:12 +0100

    16-01-2024-1350

--- Matrix_based_74.py
+++ Matrix_based_74.py
@@ -1,23 +1,59 @@
-from Class_vectors_and_matrices import vector
-from Class_vectors_and_matrices import matrix
+from Class_vectors_and_matrices import vector 
+from Class_vectors_and_matrices import matrix 
+#DEFINING TERMS#
 #secures message and adds 4 extra bits:
-Generator_matrix=matrix([[1,0,0,0,1,1,0],[0,1,0,0,1,0,1],[0,0,1,0,0,1,1],[0,0,0,1,1,1,1]]) 
+Generator_matrix=matrix([[1,0,0,0,1,1,0],[0,1,0,0,1,0,1],[0,0,1,0,0,1,1],
+                         [0,0,0,1,1,1,1]]) 
 #checks whether or not a message was corrupted
 Parity_check=matrix([[1,1,0,1,1,0,0],[1,0,1,1,0,1,0],[0,1,1,1,0,0,1]])
-test1=matrix([[1],[1],[0],[1],[1],[0],[0]])
+test1=matrix([[1],[0],[0],[1],[1],[0],[0]])
 test2=matrix([[1,1,0,1]])
 
+#Some helpful functions
 def encrypt74(self,Generator_matrix):
+    """
+    This functions takes a piece of binary code, given in the form of a matrix 
+    with size 1 x n. It returns the same code with after it the security-bits. 
+    In the form of a matrix size 1 x (n+the security-bits).
+    """
     if self.shape()[1] == Generator_matrix.shape()[0]:
         return matrix.XOR(self,Generator_matrix)
     else:
         return "This is undefined.."
-def parity74 (self,Parity_check):
-    if Parity_check.shape()[1]==self.shape()[0]:
-        answer=matrix.XOR(Parity_check,self)
-        Checking_library={
-            matrix([[0],[0],[0],[0]]) : None
-            }
+    
+def parity(check,Parity_check):
+    """
+    This functions takes a piece of binary code, given in the form of a matrix 
+    with size 1 x n. At which index the original code should be flipped, if 
+    neccesary. 
+    """
+    if Parity_check.shape()[1]==check.shape()[0]:
+        answer=matrix.XOR(Parity_check,check)
     else:
         return "This is undefined.."
-print(parity74(test1, Generator_matrix))
+    if answer == matrix([[0], [0], [0]]):
+        return None
+    elif answer== matrix([[1],[1],[0]]):
+        return 0
+    elif answer== matrix([[1],[0],[1]]):
+        return 1
+    elif answer== matrix([[0],[1],[1]]):
+        return 2
+    elif answer== matrix([[1],[1],[1]]):
+        return 3
+    elif answer== matrix([[1],[0],[0]]):
+        return 4
+    elif answer== matrix([[0],[1],[0]]):
+        return 5
+    elif answer== matrix([[0],[0],[1]]):
+        return 6
+def reperation(check,Parity_check):
+    """
+    Repares a gives vector. Question het to solve: how do we adapt an element 
+    in a matrix.
+    """
+    repare=parity(check,Parity_check)
+    return repare
+x=encrypt74(matrix([[1,1,0,0]]), Generator_matrix)
+print(matrix([1,1,0,0]))
+print(x)
